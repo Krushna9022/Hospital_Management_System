4b2205f93e0d4bfbee2201fd859bb0c34257d039
Gaurav-1A <gauravitware43@gmail.com>
2025-06-10 14:18:56 +0530

    Design login and rgister page and write api

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express=require('express')
 const app=express();
+const path=require("path");
 app.set("view engine","ejs");
 
+app.use(express.static(path.join(__dirname,"../public")));
 
 
 app.get('/',(req,res)=>{
@@ -11,4 +13,8 @@ app.get('/',(req,res)=>{
 app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
+app.get("/register",(req,res)=>{
+    res.render("register.ejs");
+})
+
 module.exports=app;
