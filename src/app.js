4e647766cdd34994a3869531644365e5796ef115
Gaurav-1A <gauravitware43@gmail.com>
2025-06-10 14:34:32 +0530

    Login (#4)
    * making login design
    
    * Design login and rgister page and write api
    
    ---------
    
    Co-authored-by: Krushna9022 <158745029+Krushna9022@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 const express=require('express')
 const app=express();
+const path=require("path");
+app.set("view engine","ejs");
+
+app.use(express.static(path.join(__dirname,"../public")));
 
 const homerouter=require('./routes/homeRoutes')
 app.set('view engine','ejs')
@@ -8,4 +12,9 @@ app.use('/',homerouter)
 app.get("/login",(req,res)=>{
     res.render("login.ejs");
 })
+
+app.get("/register",(req,res)=>{
+    res.render("register.ejs");
+})
+
 module.exports=app;
