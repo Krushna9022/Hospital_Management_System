96cdf7193ca0074e9e073e715342784caaeefd64
Gaurav-1A <gauravitware43@gmail.com>
2025-06-10 11:12:01 +0530

    making login design

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express=require('express')
 const app=express();
+app.set("view engine","ejs");
+
 
 
 app.get('/',(req,res)=>{
     res.send('hello world');
 })
 
+app.get("/login",(req,res)=>{
+    res.render("login.ejs");
+})
 module.exports=app;
