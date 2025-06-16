728de81938bab4ed79383715b10c6795dde08bdb
Gaurav-1A <gauravitware43@gmail.com>
2025-06-16 22:32:23 +0530

    update delete done on view doctor page

--- src/controllers/DoctorController.js
+++ src/controllers/DoctorController.js
@@ -4,9 +4,37 @@ exports.viewDoctor = async (req, res) => {
     try {
         let result = await doctorModels.getAllDoctor();
         // res.send(result)
-       res.render("viewdoctor", { doctors: result });  
+        console.log(result);
+        console.log("All doctors ");
+        
+       res.render("viewDoctor", { doctors: result });  
     //  use: res.json(result); 
     } catch (err) {
         res.status(500).send("Error fetching doctor data.");
     }
 };
+
+
+
+exports.deleteDoctorController=async (req,res)=>{
+
+    const userId=parseInt(req.params.userId);
+    await doctorModels.deleteDoctorById(userId)
+    res.redirect("/api/admin/viewDoctor");
+}
+
+exports.getUpdatedoctorpage=async(req,res)=>{
+     const { userId } =req.params;
+     let doctor=await doctorModels.getdoctorById(userId)
+        if(!doctor){ return res.status(404).send("doctor not found");}
+          res.render("updateDoctor.ejs",{rec:doctor})
+        // res.send(receptionist)
+};
+
+exports.updateDoctor=async(req,res)=>{
+  const{userId,name,email,contact,experience,specialization}=req.body;
+  console.log(req.body);
+  
+  await doctorModels.updateDoctor(userId,name,email,contact,experience,specialization);
+  res.redirect('/api/admin/viewDoctor');
+}
--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -1,7 +1,10 @@
 const express=require("express");
 const router=express.Router();
-const doctorController=require("../controllers/doctorController");
+const doctorController=require("../controllers/DoctorController");
 
 router.get("/viewDoctor",doctorController.viewDoctor);
+router.get("/deleteDoctor/:userId",doctorController.deleteDoctorController);
 
+router.get('/updatedoctor/:userId',doctorController.getUpdatedoctorpage);
+router.post('/updatedoctors',doctorController.updateDoctor)
 module.exports=router;
