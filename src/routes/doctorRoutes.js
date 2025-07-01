c554cad45fb539c2b918ea4855306f1bc6fc0e01
Krushna9022 <158745029+Krushna9022@users.noreply.github.com>
2025-07-02 00:48:07 +0530

    fixed and format code (#29)

--- src/routes/doctorRoutes.js
+++ src/routes/doctorRoutes.js
@@ -5,7 +5,6 @@ const { isAuthenticated, authorizeRole } = require("../middlewares/auth");
 
 router.get("/viewDoctors",isAuthenticated,authorizeRole("admin"), doctorController.viewDoctor);
 router.get("/deleteDoctor/:userId",isAuthenticated,authorizeRole("admin"),doctorController.deleteDoctorController);
-
 router.get('/updatedoctor/:userId',isAuthenticated,authorizeRole("admin"),doctorController.getUpdatedoctorpage);
 router.post('/updatedoctors',isAuthenticated,authorizeRole("admin"),doctorController.updateDoctor)
 module.exports=router;
